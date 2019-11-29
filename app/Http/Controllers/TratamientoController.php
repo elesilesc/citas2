bab02a737b9aeb5b84799c155e193b9d850f127f
elesilesc <elesilesc@alum.us.es>
2019-11-29 14:37:13 +0100

    Actualizaciones Elena

---
+++ app/Http/Controllers/TratamientoController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tratamiento;
+use Illuminate\Http\Request;
+
+class TratamientoController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tratamientos = Tratamiento::all();
+        return view('tratamientos/index',['tratamientos'=>$tratamientos]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $citas = Cita::all()->pluck('fecha_hora','id');
+
+        return view('tratamientos/create',['citas'=>$citas]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'initTime' => 'required|date|after:now',
+            'endTime' => 'required|date|after:now',
+            'descripcion' => 'required|max:255',
+            'units' => 'required|max:255',
+            'frecuencia' => 'required|max:255',
+            'instrucciones' => 'required|max:255',
+            'cita_id' => 'required|exists:citas,id'
+        ]);
+        $tratamiento = new Tratamiento($request->all());
+        $tratamiento->save();
+
+
+        flash('Tratamiento creado correctamente');
+
+        return redirect()->route('tratamientos.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tratamiento  $tratamiento
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tratamiento  $tratamiento
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tratamiento $tratamiento)
+    {
+        $tratamiento = Tratamiento::find($id);
+        $cita = Cita::all()->pluck('fecha_hora','id');
+        return view('tratamientos/edit',['tratamiento'=> $tratamiento, 'citas'=>$citas ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tratamiento  $tratamiento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tratamiento $tratamiento)
+    {
+        $this->validate($request, [
+            'initTime' => 'required|date|after:now',
+            'endTime' => 'required|date|after:now',
+            'descripcion' => 'required|max:255',
+            'units' => 'required|max:255',
+            'frecuencia' => 'required|max:255',
+            'instrucciones' => 'required|max:255',
+            'cita_id' => 'required|exists:citas,id'
+        ]);
+
+        $tratamiento = Tratamiento::find($id);
+        $tratamiento->fill($request->all());
+        $tratamiento->save();
+
+        flash('Tratamiento modificado correctamente');
+
+        return redirect()->route('tratamientos.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tratamiento  $tratamiento
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tratamiento $tratamiento)
+    {
+        $tratamiento = Tratamiento::find($id);
+        $tratamiento->delete();
+        flash('Tratamiento borrado correctamente');
+
+        return redirect()->route('tratamiento.index');
+    }
+}
---
+++ resources/views/tratamientos/edit.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Editar tratamiento</div>
+
+                    <div class="panel-body">
+                        @include('flash::message')
+
+                        {!! Form::model($tratamiento, [ 'route' => ['tratamientos.update',$tratamiento->id], 'method'=>'PUT']) !!}
+
+                        <div class="form-group">
+                            {!! Form::label('initTime', 'Fecha de inicio') !!}
+                            {!! Form::text('initTime',$tratamiento->initTime,['class'=>'form-control', 'required', 'autofocus']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('endTime', 'Fecha de fin') !!}
+                            {!! Form::text('endTime',$tratamiento->endTime,['class'=>'form-control', 'required']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('descripcion', 'Descripcion del tratamiento') !!}
+                            {!! Form::text('descripcion',$tratamiento->descripcion,['class'=>'form-control', 'required']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('units', 'Unidades del medicamento') !!}
+                            {!! Form::text('units',$tratamiento->units,['class'=>'form-control', 'required']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('frecuencia', 'Frecuencia') !!}
+                            {!! Form::text('frecuencia',$tratamiento->frecuencia,['class'=>'form-control', 'required']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('instrucciones', 'Instrucciones al paciente') !!}
+                            {!! Form::text('instrucciones',$tratamiento->instrucciones,['class'=>'form-control', 'required']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!!Form::label('cita_id', 'Cita paciente') !!}
+                            <br>
+                            {!! Form::select('cita_id', $citas, $tratamiento->cita_id, ['class' => 'form-control', 'required']) !!}
+                        </div>
+                        {!! Form::submit('Guardar',['class'=>'btn-primary btn']) !!}
+
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
